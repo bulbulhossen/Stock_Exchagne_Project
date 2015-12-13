ec7f0d5b313acc2dc3d2c372a58e142f65075ac4
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-13 22:15:00 +0600

    Add -- change Password screen

--- app/src/main/java/com/example/bulbulhossen/dse_project/Fragment/Account_Fragment.java
+++ app/src/main/java/com/example/bulbulhossen/dse_project/Fragment/Account_Fragment.java
@@ -1,18 +1,23 @@
 package com.example.bulbulhossen.dse_project.Fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.bulbulhossen.dse_project.Home_page.Change_password;
 import com.example.bulbulhossen.dse_project.R;
 
 /**
  * Created by Md.Bulbul Hossen on 11/12/15.
  */
 public class Account_Fragment extends Fragment {
+
+    Button change_pass;
 
     public Account_Fragment() {
         // Required empty public constructor
@@ -28,6 +33,16 @@ public class Account_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.account_setting_fragment, container, false);
+
+
+        change_pass = (Button) rootView.findViewById(R.id.button3);
+        change_pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Change_password.class);
+                startActivity(intent);
+            }
+        });
 
 
         // Inflate the layout for this fragment
